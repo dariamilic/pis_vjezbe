192d9eb818f934b5a1e50dafa503d273d816ddc7
lucija-26 <92396288+lucija-26@users.noreply.github.com>
2024-03-19 20:19:35 +0100

    Updated Program.cs

--- pis_vjezbe/Program.cs
+++ pis_vjezbe/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World2 hejjj!");
+Console.WriteLine("Hello, World3 hejjj!");
